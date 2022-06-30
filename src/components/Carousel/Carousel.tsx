0127541d5d0daed306bb2498348b73846511f543
yuanguangbin <786497187@qq.com>
2022-06-30 17:02:40 +0800

    carousel

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,10 +1,16 @@
-import React, { Children, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import classNames from "classnames";
 
 export type triggerType = 'click' | 'hover';
 export type indicatorPositionType = 'none' | 'outside';
 export type displayArrowTime = 'always' | 'hover' | 'never';
 export type CarouselType = 'card' | 'flatcard';
+interface Item {
+  /** 图片地址 */
+  url: string;
+  /** 图片的alt属性 */
+  alt?: string;
+}
 interface CarouselProps {
   /** 轮播图高度 */
   height?: number;
@@ -22,7 +28,7 @@ interface CarouselProps {
   arrow?: displayArrowTime;
   /** 轮播图的类型，可选值'always' | 'hover' | 'never'*/
   type?: CarouselType;
-  children?: React.ReactNode;
+  list?: Item[];
 }
 
 export const Carousel: React.FC<CarouselProps> = (props) => {
@@ -35,14 +41,74 @@ export const Carousel: React.FC<CarouselProps> = (props) => {
     indicatorPosition,
     arrow,
     type,
-    children
   } = props;
-  const [ index, setIndex ] = useState(initialIndex);
+  const list = props.list || [];
+  if(list.length === 0){
+    console.warn("The length of list shouldn't be less than 1");
+  }
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [ containerWidth, setContrainerWidth ] = useState<number>(0);
+  const [ currentIndex, setCurrentIndex ] = useState(initialIndex || 0);
+  const arrowclass = classNames('arrow', `arrow-${arrow}`);
 
+  useEffect(() => {
+    if(containerRef.current) {
+      setContrainerWidth(containerRef.current.offsetWidth)
+      console.log(containerWidth)
+    }
+  }, [list.length])
+
+  const carouselItem = list.map((item, index) => {
+    const url = item.url;
+    const item_img_class = classNames('carousel-item', {
+      'is-actived': index === currentIndex,
+    })
+    return (
+      <li
+        key={index}
+        className={item_img_class}
+      ><img src={url}/></li>
+    )
+  })
+
+
+  const wrapperStyle = {
+    width: `${list.length}00%`,
+    transform: `translateX(-${containerWidth * currentIndex}px)`,
+    transition: `transform 1s ease-in`,
+  }
+
+  // 切换页面的函数
+  const handleChange = (isNext: boolean) => {
+    if(isNext) {
+      if(currentIndex < list.length - 1) {
+        setCurrentIndex(currentIndex + 1);
+      } else {
+        setCurrentIndex(0);
+      }
+    } else {
+      if(currentIndex > 0) {
+        setCurrentIndex(currentIndex - 1);
+      } else {
+        setCurrentIndex(list.length - 1);
+      }
+    }
+  }
 
   return (
-    <div>
-      {children}
+    <div className="yuan-carousel-container" ref={containerRef}>
+      <ul className="img" style={wrapperStyle}>
+        { carouselItem }
+      </ul>
+
+      <ul className="pointer">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <div className="arrow-l" onClick={() => handleChange(false)}>&lt;</div>
+      <div className="arrow-r" onClick={() => handleChange(true)}>&gt;</div>
     </div>
   )
 };
---
+++ src/components/Carousel/carousel.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Carousel from "./Carousel";
+
+const list = [
+  { url: 'http://p1.music.126.net/IXb-PeUMyeFDA1KHQraF4Q==/109951167605258627.jpg'},
+  { url: 'http://p1.music.126.net/Lju50bXS-hKojXULndnzew==/109951167605266134.jpg'},
+  { url: 'http://p1.music.126.net/oDweleFdUs69qYbV1vFvOA==/109951167604823899.jpg'},
+  { url: 'http://p1.music.126.net/tSI5IA4Q4oB0nN2voDObfA==/109951167605477857.jpg'},
+];
+
+export default {
+  title: 'Carousel',
+  component: Carousel,
+  argTypes: {
+  }
+} as ComponentMeta<typeof Carousel>;
+
+const Template: ComponentStory<typeof Carousel> = (args) => <Carousel {...args}></Carousel>
+
+export const DefaultCarousel = Template.bind({});
+
+DefaultCarousel.args = {
+  list: list,
+}
